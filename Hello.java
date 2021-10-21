f3bee88a3a1509e9add386abb111f52d12dcd7f5
unknown <gayoyawalkar@gmail.com>
2021-10-21 07:16:08 +0530

    Welcome to user registration with lambda expression

---
+++ Hello.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.lambdaexpression;
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration Program with Lambda Expression");
+	}
+}
